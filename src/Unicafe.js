779993ec6b1b4ff003725d0641002599b278a9d4
Veera Koskinen <veera.koskinen@ren.to>
2018-01-10 14:15:04 +0200

    Tehtävä 13

--- src/Unicafe.js
+++ src/Unicafe.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 class Unicafe extends React.Component {
 
     constructor(props) {
@@ -19,24 +21,41 @@ class Unicafe extends React.Component {
         this.setState({counter: 0})
     }
 
+    precisionRound(number, precision) {
+        var factor = Math.pow(10, precision);
+        return Math.round(number * factor) / factor;
+      }
 
-    render() {
+    statistics() {
+        const allAnswers=this.state.badCounter + this.state.okCounter + this.state.goodCounter
+        const average=(this.state.goodCounter - this.state.badCounter)/allAnswers
+        const percentage=this.state.goodCounter/allAnswers
         return (
             <div>
-                <h1>anna palautetta</h1>
-                
-                <button onClick={() => this.addOne("goodCounter")}>hyvä</button>
-                <button onClick={() => this.addOne("okCounter")}>neutraali</button>
-                <button onClick={() => this.addOne("badCounter")}>huono</button>
-
                 <h1>statistiikka</h1>
                 <div>hyvä: {this.state.goodCounter}</div>
                 <div>neutraali: {this.state.okCounter}</div>
                 <div>huono: {this.state.badCounter}</div>
+                <div>keskiarvo: {this.precisionRound(average,2)}</div>
+                <div>positiivisia: {this.precisionRound(percentage,2)} %</div>
+            </div>    
+        )
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>anna palautetta</h1>               
+                <button onClick={() => this.addOne("goodCounter")}>hyvä</button>
+                <button onClick={() => this.addOne("okCounter")}>neutraali</button>
+                <button onClick={() => this.addOne("badCounter")}>huono</button>
+                {this.statistics()}           
             </div>
         )
     }
 }
+
 
 export default Unicafe
 
