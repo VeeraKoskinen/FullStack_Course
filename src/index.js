bf437142e6f708851f826eb5d380713340998c5d
Veera Koskinen <veera.koskinen@ren.to>
2018-01-10 22:10:19 +0200

    Tehtävä 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Unicafe from './Unicafe'
+import Anekdootit from './Anekdootit'
 
 
 const Otsikko = (props) => {
@@ -55,7 +56,7 @@ const App = () => {
           tehtavia: 14
         }
     ]
-}
+    }
   return (
       <div>
         <Otsikko kurssi={kurssi}/>
@@ -66,7 +67,17 @@ const App = () => {
  
 }
 
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 ReactDOM.render(
-  <Unicafe />,
+  <Anekdootit anecdotes={anecdotes}/>,
   document.getElementById('root')
 )
