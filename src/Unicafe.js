1f8c57134b516a91d3f18ca42965f7e6cf4829b3
Veera Koskinen <veera.koskinen@ren.to>
2018-01-10 17:14:57 +0200

    Tehtävä 14

--- src/Unicafe.js
+++ src/Unicafe.js
@@ -1,6 +1,42 @@
 import React from 'react'
 
+const precisionRound = (number, precision) => {
+    var factor = Math.pow(10, precision);
+    return Math.round(number * factor) / factor;
+}
 
+const Button = (props) => {
+    return (
+        <button onClick={props.function}>{props.text}</button>
+    )
+}
+
+const Statistic = (props) => {
+    return (
+        <div>{props.name} {props.number} {props.text}</div>
+    )
+}
+
+const Statistics = (props) => {
+    const allAnswers = props.bad + props.ok + props.good
+    const average = (props.good - props.bad)/allAnswers
+    const percentage = props.good/allAnswers
+        
+    const name1 = "hyvä: ";
+    const name2 = "neutraali: ";
+    const name3 = "huono: ";
+    return (
+        <div>
+            <h1>statistiikka</h1>
+            <Statistic name={name1} number={props.good}/>
+            <Statistic name={name2} number={props.ok}/>
+            <Statistic name={name3} number={props.bad}/>
+            <Statistic name="keskiarvo: " number={precisionRound(average,2)}/>
+            <Statistic name="positiivisia: " number={precisionRound(percentage,2)} text="%"/>
+        </div>    
+
+    )
+}
 
 class Unicafe extends React.Component {
 
@@ -21,36 +57,14 @@ class Unicafe extends React.Component {
         this.setState({counter: 0})
     }
 
-    precisionRound(number, precision) {
-        var factor = Math.pow(10, precision);
-        return Math.round(number * factor) / factor;
-      }
-
-    statistics() {
-        const allAnswers=this.state.badCounter + this.state.okCounter + this.state.goodCounter
-        const average=(this.state.goodCounter - this.state.badCounter)/allAnswers
-        const percentage=this.state.goodCounter/allAnswers
-        return (
-            <div>
-                <h1>statistiikka</h1>
-                <div>hyvä: {this.state.goodCounter}</div>
-                <div>neutraali: {this.state.okCounter}</div>
-                <div>huono: {this.state.badCounter}</div>
-                <div>keskiarvo: {this.precisionRound(average,2)}</div>
-                <div>positiivisia: {this.precisionRound(percentage,2)} %</div>
-            </div>    
-        )
-    }
-
-
     render() {
         return (
             <div>
-                <h1>anna palautetta</h1>               
-                <button onClick={() => this.addOne("goodCounter")}>hyvä</button>
-                <button onClick={() => this.addOne("okCounter")}>neutraali</button>
-                <button onClick={() => this.addOne("badCounter")}>huono</button>
-                {this.statistics()}           
+                <h1>anna palautetta</h1>
+                <Button text="hyvä" function={() => this.addOne("goodCounter")} /> 
+                <Button text="neutraali" function={() => this.addOne("okCounter")} />
+                <Button text="huono" function={() => this.addOne("badCounter")} />
+                <Statistics bad={this.state.badCounter}  good={this.state.goodCounter} ok={this.state.okCounter} />          
             </div>
         )
     }
